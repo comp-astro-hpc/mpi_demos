3b59cf99a04c74af056556548e09d93e0bc5674b
Ritali Ghosh <ritsg11@gmail.com>
2023-07-10 23:38:45 +0530

    added demo files

--- broadcast.py
+++ broadcast.py
@@ -5,12 +5,18 @@ prank = comm.Get_rank()
 size  = comm.Get_size()
 
 # create a dummy dictionary for only rank=0
-
+if prank==0:
+   data = {'nums': [1, 2.5, 3.5],
+          'alph': ('a', 'ad', 'bcv')
+         }
+else:
+   data = None
+print("Before bcast rank %d : "%prank, data, flush=True)
 # broadcast the dictionary to all other ranks
-
+data = comm.bcast(data, root=0)
+print("After Bcast %d:  "%prank, data, flush=True)
 # print broadcasted data received by all ranks
 
 # NOTE:don't forget to flush stdout for immediate display
-
 if MPI.Is_initialized():
    MPI.Finalize()
--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,13 @@ comm  = MPI.COMM_WORLD
 prank = comm.Get_rank()
 size  = comm.Get_size()
 
-# write your code
+data = None
+if prank == 0:
+    data = {'a': 7, 'b': 3.14}
+    comm.send(data, dest=1, tag=11)
+elif prank == 1:
+    data = comm.recv(source=0, tag=11)
+print("Rank %d has"%prank, data)
 
 if MPI.Is_initialized():
    MPI.Finalize()
