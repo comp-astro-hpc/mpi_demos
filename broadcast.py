3dc2667982e730646aafa9fc70a100edd2ae4df0
Ritali Ghosh <ritsg11@gmail.com>
2023-07-11 00:04:57 +0530

    final updates

--- broadcast.py
+++ broadcast.py
@@ -1,10 +1,9 @@
 from mpi4py import MPI
 
-comm  = MPI.COMM_WORLD
+comm = MPI.COMM_WORLD
 prank = comm.Get_rank()
-size  = comm.Get_size()
 
-# create a dummy dictionary for only rank=0
+#create a dummy dictionary for only rank=0
 if prank==0:
    data = {'nums': [1, 2.5, 3.5],
           'alph': ('a', 'ad', 'bcv')
@@ -12,11 +11,9 @@ if prank==0:
 else:
    data = None
 print("Before bcast rank %d : "%prank, data, flush=True)
+
 # broadcast the dictionary to all other ranks
 data = comm.bcast(data, root=0)
-print("After Bcast %d:  "%prank, data, flush=True)
-# print broadcasted data received by all ranks
 
-# NOTE:don't forget to flush stdout for immediate display
-if MPI.Is_initialized():
-   MPI.Finalize()
+# print broadcasted data received by all ranks
+print("After Bcast %d:  "%prank, data, flush=True)
--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,21 @@
 from mpi4py import MPI
+import numpy as np
 
 comm  = MPI.COMM_WORLD
 prank = comm.Get_rank()
 size  = comm.Get_size()
 
+# write your code
 data = None
 if prank == 0:
-    data = {'a': 7, 'b': 3.14}
-    comm.send(data, dest=1, tag=11)
-elif prank == 1:
-    data = comm.recv(source=0, tag=11)
-print("Rank %d has"%prank, data)
+    data = np.array([0., 1.2, 3.5])
+    print("Rank %d has"%prank, data, flush=True)
+    # process zero sends this to all 
+    for pid in range(1, size):
+       comm.send(data, dest=pid)
+else:
+    data = comm.recv(source=0)
+    print("Rank %d has"%prank, data, flush=True)
 
 if MPI.Is_initialized():
    MPI.Finalize()
